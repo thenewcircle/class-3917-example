b4118bfd412f1eacbb3a86c064bc3893e2b0803f
Doug Bateman <doug@dougbateman.net>
2015-02-19 22:12:49 -0500

    Feature: Adding resetFancy to ConfigurationService.

--- src/main/java/com/example/di/ConfigurationService.java
+++ src/main/java/com/example/di/ConfigurationService.java
@@ -4,7 +4,11 @@ import javax.sql.DataSource;
 
 public abstract class ConfigurationService {
 
-	private static ConfigurationService instance = new ConfigurationServicePlain();
+	private static ConfigurationService instance;
+	
+	static {
+		reset();
+	}
 
 	public static synchronized ConfigurationService getInstance() {
 		return instance;
@@ -14,8 +18,39 @@ public abstract class ConfigurationService {
 		ConfigurationService.instance = service;
 	}
 	
-	public static synchronized void reset() {
-		ConfigurationService.instance = new ConfigurationServicePlain();
+	public static void reset() {
+		ConfigurationService config = new ConfigurationServicePlain();
+		setInstance(config);
+	}
+	
+	/**
+	 * This is a fancier version that allows for overriding the config via a
+	 * system property. You probably don't need this added complexity, but we
+	 * wanted to show it's possible.
+	 */
+	public static void resetFancy() {
+		try {
+			ConfigurationService config = createConfig();
+			setInstance(config);
+		} catch (RuntimeException | Error e) {
+			setInstance(null);
+			throw e;
+		}
+	}
+
+	private static ConfigurationService createConfig() {
+		String configClassName = System.getProperty("com.example.config.class");
+		if (configClassName == null) {
+			ConfigurationService config = new ConfigurationServicePlain();
+			return config;
+		}
+		try {
+			Class<? extends ConfigurationService> configClass = Class.forName(configClassName).asSubclass(ConfigurationService.class);
+			ConfigurationService config = configClass.newInstance();
+			return config;
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to create config: " + configClassName, e);
+		}
 	}
 
 	public abstract CustomerService getCustomerService();
