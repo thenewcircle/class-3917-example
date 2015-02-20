f82dae0598fea43d1c22dbb2a0b8fc968edd2ce2
JacobP <JacobP@PARISJACOBPLT.STCG.NET>
2015-02-19 22:59:08 -0500

    Feature: Implemented two spring initialization versions

--- src/main/java/com/example/di/ConfigurationServiceSpringJavaConfig.java
+++ src/main/java/com/example/di/ConfigurationServiceSpringJavaConfig.java
@@ -2,58 +2,102 @@ package com.example.di;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ConfigurationServiceSpringJavaConfig extends ConfigurationService {
 
-	private ApplicationContext spring;
-
-	public ConfigurationServiceSpringJavaConfig() {
-		//load spring here...
+	public static ConfigurationServiceSpringJavaConfig install() {
+		return install("/com/example/di/spring-config-injected.xml");
+	}
+	
+	public static ConfigurationServiceSpringJavaConfig install(String xmlFile) {
+		ConfigurationServiceSpringJavaConfig config = new ConfigurationServiceSpringJavaConfig(xmlFile);
+		ConfigurationService.setInstance(config);
+		return config;
 	}
 
+	@Autowired
+	@Qualifier("regional")
+	private DataSource regionalDataSource;
+
+	@Autowired
+	@Qualifier("global")
+	private DataSource globalDataSource;
+
+	@Autowired
+	private CustomerService customerService;
+
+	@Autowired
+	private NetworkAddrService networkAddrService;
+
+	@Autowired
+	private SecurityService securityService;
+
+	@Autowired
+	private DistributionEngine distributionEngine;
+
+	@Autowired
+	private MyValidator myValidator;
+
+	@Autowired
+	private MyStatelessWorkerBean worker;
+
+	@Autowired
+	private RequestMessageLogService requestMessageLogService;
+
+	@SuppressWarnings("deprecation")
+	public ConfigurationServiceSpringJavaConfig(String xmlFile) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(xmlFile); 
+		ctx.getAutowireCapableBeanFactory().autowireBean(this);
+	}
+
+	@Override
 	public CustomerService getCustomerService() {
-		return spring.getBean("customerService", CustomerService.class);
+		return customerService;
 	}
 
+	@Override
 	public NetworkAddrService getNetworkAddrService() {
-		//TODO
-		return null;
+		return networkAddrService;
 	}
 
+	@Override
 	public SecurityService getSecurityService() {
-		//TODO
-		return null;
+		return securityService;
 	}
 
+	@Override
 	public DistributionEngine getDistributionEngine() {
-		//TODO
-		return null;
+		return distributionEngine;
 	}
 
+	@Override
 	public MyValidator getMyValidator() {
-		//TODO
-		return null;
+		return myValidator;
 	}
 
+	@Override
 	public MyStatelessWorkerBean getWorker() {
-		//TODO
-		return null;
+		return worker;
 	}
 
+	@Override
 	public DataSource getRegionalDataSource() {
-		//TODO
-		return null;
+		return regionalDataSource;
 	}
 
+	@Override
 	public DataSource getGlobalDataSource() {
-		//TODO
-		return null;
+		return globalDataSource;
 	}
 
+	@Override
 	public RequestMessageLogService getRequestMessageLogService() {
-		//TODO
-		return null;
+		return requestMessageLogService;
 	}
 	
 }
--- src/main/java/com/example/di/ConfigurationServiceSpringXML.java
+++ src/main/java/com/example/di/ConfigurationServiceSpringXML.java
@@ -2,58 +2,72 @@ package com.example.di;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ConfigurationServiceSpringXML extends ConfigurationService {
 
-	private ApplicationContext spring;
-
-	public ConfigurationServiceSpringXML() {
-		//load spring here...
-	}
-
-	public CustomerService getCustomerService() {
-		return spring.getBean("customerService", CustomerService.class);
-	}
-
-	public NetworkAddrService getNetworkAddrService() {
-		//TODO
-		return null;
-	}
-
-	public SecurityService getSecurityService() {
-		//TODO
-		return null;
-	}
-
-	public DistributionEngine getDistributionEngine() {
-		//TODO
-		return null;
-	}
-
-	public MyValidator getMyValidator() {
-		//TODO
-		return null;
-	}
-
-	public MyStatelessWorkerBean getWorker() {
-		//TODO
-		return null;
-	}
-
-	public DataSource getRegionalDataSource() {
-		//TODO
-		return null;
-	}
-
-	public DataSource getGlobalDataSource() {
-		//TODO
-		return null;
-	}
-
-	public RequestMessageLogService getRequestMessageLogService() {
-		//TODO
-		return null;
+	public static ConfigurationServiceSpringXML install() {
+		return install("/com/example/di/spring-config-factory.xml");
 	}
 	
+	public static ConfigurationServiceSpringXML install(String xmlFile) {
+		ConfigurationServiceSpringXML config = new ConfigurationServiceSpringXML(xmlFile);
+		ConfigurationService.setInstance(config);
+		return config;
+	}
+
+	private final ApplicationContext spring;
+	
+	public ConfigurationServiceSpringXML(String xmlFile) {
+		spring = new ClassPathXmlApplicationContext(xmlFile); 
+	}
+
+	@Override
+	public CustomerService getCustomerService() {
+		return spring.getBean(CustomerService.class);
+	}
+
+	@Override
+	public NetworkAddrService getNetworkAddrService() {
+		return spring.getBean(NetworkAddrService.class);
+	}
+
+	@Override
+	public SecurityService getSecurityService() {
+		return spring.getBean(SecurityService.class);
+	}
+
+	@Override
+	public DistributionEngine getDistributionEngine() {
+		return spring.getBean(DistributionEngine.class);
+	}
+
+	@Override
+	public MyValidator getMyValidator() {
+		return spring.getBean(MyValidator.class);
+	}
+
+	@Override
+	public MyStatelessWorkerBean getWorker() {
+		return spring.getBean(MyStatelessWorkerBean.class);
+	}
+
+	@Override
+	public DataSource getRegionalDataSource() {
+		return spring.getBean("regionalDataSource", DataSource.class);
+	}
+
+	@Override
+	public DataSource getGlobalDataSource() {
+		return spring.getBean("globalDataSource", DataSource.class);
+	}
+
+	@Override
+	public RequestMessageLogService getRequestMessageLogService() {
+		return spring.getBean(RequestMessageLogService.class);
+	}
 }
